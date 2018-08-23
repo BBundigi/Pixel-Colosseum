2d9e6363b5883ba62c695dd00cd79169aba4cfa8
Test <dl120600@gmail.com>
2018-08-23 23:02:55 +0900

    /

---
+++ Assets/Script/GameController/BGController_Lobby.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGController_Lobby : MonoBehaviour {
+    private Rigidbody2D Rb;
+    [SerializeField]
+    private Vector2 ResetPoint;
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private string Tag;
+	// Use this for initialization
+	void Start () {
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.velocity = Speed * Vector2.down;
+	}
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if(Other.CompareTag(Tag))
+        {
+            Rb.position = ResetPoint;
+        }
+    }
+}
